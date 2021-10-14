e98c71a7b76eb23608215eab5e1744ae1335d704
Miqueias Meneses <57450216+mm-silva@users.noreply.github.com>
2021-10-13 22:59:04 -0300

    Fix mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Teste mensagem customizada..."
+    return "Teste mensagem customizada"
 
 if __name__ == '__main__':
     app.run(debug=True)
